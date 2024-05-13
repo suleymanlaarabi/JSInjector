ea0f68bcdd56893cd525df55db2b4c524e84b03a
suleymanlaarabi <suleyman.laarabi.dev@gmail.com>
2024-05-13 14:34:49 +0200

    change return to paht buffer

--- src/features/select_script.rs
+++ src/features/select_script.rs
@@ -1,26 +1,23 @@
+use std::path::{Path, PathBuf};
+
 use dialoguer::Input;
-use tokio::fs;
 
 use crate::utils::stdout_utils::clear_stdout_up;
 
-pub async fn select_script() -> String {
+pub async fn select_script() -> PathBuf {
     loop {
         let path: String = Input::new()
             .with_prompt("Path of your script ? ")
             .interact_text()
             .unwrap();
 
-        let script_content = fs::read_to_string(&path).await;
+        let script_path = Path::new(&path).to_owned();
+        let exist_file = script_path.is_file();
+
         clear_stdout_up();
-        match script_content {
-            Ok(content) => {
-                println!("Script content: {}", content);
-                return content;
-            }
-            Err(_) => {
-                println!("Unable to read the file");
-                continue;
-            }
+        if exist_file {
+            return script_path;
         }
+        println!("Unable to read the scrip");
     }
 }
