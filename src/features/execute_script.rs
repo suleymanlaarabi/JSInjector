188123fc381bca2206d853a51ead324ebe0f2a41
suleymanlaarabi <suleyman.laarabi.dev@gmail.com>
2024-05-13 14:33:35 +0200

    feat: hot refresh

--- src/features/execute_script.rs
+++ src/features/execute_script.rs
@@ -1,37 +1,111 @@
-use std::time::Duration;
+use std::{
+    fs,
+    path::PathBuf,
+    time::{Duration, Instant},
+};
 
-use futures::{stream::StreamExt, SinkExt};
+use crate::{prelude::DebuggerInfo, utils::stdout_utils::clear_stdout};
+use console::Term;
+use dialoguer::{theme::ColorfulTheme, Select};
+use futures::{
+    channel::mpsc::{channel, Receiver},
+    stream::StreamExt,
+    SinkExt,
+};
+use notify::{Config, Event, RecommendedWatcher, RecursiveMode, Watcher};
+use serde::{Deserialize, Serialize};
 use tokio::time::sleep;
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 
-use crate::prelude::DebuggerInfo;
+pub async fn execute_script(
+    script_path: &PathBuf,
+    debugger_info: &DebuggerInfo,
+    theme: &ColorfulTheme,
+    term: &Term,
+) {
+    let menu: Vec<&str> = vec!["Hot refresh", "Just inject"];
 
-pub async fn execute_script(script: &String, debugger_info: &DebuggerInfo) {
+    let selection = Select::with_theme(theme)
+        .with_prompt("Options: ")
+        .default(0)
+        .items(&menu)
+        .interact_on_opt(&term);
+
     let (mut socket, _) = connect_async(debugger_info.get_ws_url().unwrap())
         .await
         .expect("Failed to connect");
 
-    let mut js_command = JsCommand::new();
-    js_command.add_script(script);
+    async fn execute(
+        script_path: &PathBuf,
+        socket: &mut (impl SinkExt<Message> + Unpin + StreamExt),
+    ) {
+        clear_stdout();
+        println!("script injected");
+        let binding = fs::read_to_string(script_path).unwrap();
+        let script = binding.as_str();
 
-    let message = Message::Text(js_command.to_string());
+        let mut js_command = JsCommand::new();
+        js_command.add_script(script);
 
-    println!("Sending message: {:?}", js_command.to_string());
+        let message = Message::Text(js_command.to_string());
 
-    socket.send(message).await.expect("Failed to send message");
+        let _ = socket.send(message).await;
+    }
 
-    while let Some(message) = socket.next().await {
-        let message = message.expect("Failed to receive a message");
-        if let Message::Text(text) = message {
-            println!("Response: {}", text);
-            break;
+    match selection {
+        Ok(Some(value)) => match value {
+            0 => {
+                let (mut watcher, mut rx) = async_watcher().unwrap();
+
+                watcher
+                    .watch(&script_path, RecursiveMode::Recursive)
+                    .unwrap();
+
+                let mut last_exec = Instant::now();
+                execute(&script_path, &mut socket).await;
+                while let Some(_) = rx.next().await {
+                    let now = Instant::now();
+
+                    if now.duration_since(last_exec) > Duration::from_secs(1)
+                        || now.duration_since(last_exec) < Duration::from_secs(0)
+                    {
+                        execute(&script_path, &mut socket).await;
+                        last_exec = now;
+                    }
+                }
+            }
+            1 => {
+                execute(&script_path, &mut socket).await;
+            }
+            _ => {
+                eprintln!("Invalid option");
+            }
+        },
+        Ok(None) => {
+            eprintln!("No option selected");
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e);
         }
     }
 
     sleep(Duration::from_secs(1)).await;
 }
 
-use serde::{Deserialize, Serialize};
+fn async_watcher() -> notify::Result<(RecommendedWatcher, Receiver<notify::Result<Event>>)> {
+    let (mut tx, rx) = channel(1);
+
+    let watcher = RecommendedWatcher::new(
+        move |res| {
+            futures::executor::block_on(async {
+                tx.send(res).await.unwrap();
+            })
+        },
+        Config::default(),
+    )?;
+
+    Ok((watcher, rx))
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct JsCommand {
