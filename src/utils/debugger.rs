131202f02984edf051a4f0fff559bdedddb409ae
suleymanlaarabi <suleyman.laarabi.dev@gmail.com>
2024-05-13 14:35:10 +0200

    fix: return a result of request

--- src/utils/debugger.rs
+++ src/utils/debugger.rs
@@ -1,12 +1,14 @@
 use crate::prelude::DebuggerCollection;
 
-pub async fn get_debugger_info() -> DebuggerCollection {
-    let body = reqwest::get("http://localhost:9222/json")
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap();
+pub async fn get_debugger_info() -> Result<DebuggerCollection, String> {
+    let res = reqwest::get("http://localhost:9222/json").await;
 
-    serde_json::from_str::<DebuggerCollection>(&body).unwrap()
+    match res {
+        Ok(body) => {
+            return Ok(
+                serde_json::from_str::<DebuggerCollection>(&body.text().await.unwrap()).unwrap(),
+            );
+        }
+        Err(_) => Err(String::from("Chrome is not available")),
+    }
 }
